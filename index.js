3ea1f4bd730e992f774ca12efc17c19c10c6a35e
cathytanya <cathycolette.tanya@gmail.com>
2021-10-20 23:20:12 -0400

    add require links of each the needed positions and an array on the index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // List of modules used
 const fs = require("fs")
 const inquirer = require("inquirer")
-
-
+// employee sub-classes being pulled for the team members
+const Manager = require('./teamMembers/Manager')
+const Engineer = require('./teamMembers/Engineer')
+const Intern = require('./teamMembers/Intern')
 // make an array for the entered positions the user will have
-
+const team
 // prompts for the Manager: Name, ID, Office Number, Email
 
 // prompts for the Employee(choose betwen Intern & Engineer): Name, ID, Office Number, Email
