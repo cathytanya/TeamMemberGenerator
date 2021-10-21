04ef40ee537748ca45d93aeef5c2b631887b84a3
cathytanya <cathycolette.tanya@gmail.com>
2021-10-21 00:25:24 -0400

    reinstalled module /reinitialized

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Intern = require('./teamMembers/Intern')
 // make an array for the entered positions the user will have
 const team = []
 // prompts for the Manager: Name, ID, Office Number, Email
-const addManager = () =>{
+const addManager = () => {
     return inquirer.prompt([
         {
             // prompt for manager's name
@@ -33,8 +33,21 @@ const addManager = () =>{
             type: 'input',
             message: "Enter manager's office number:",
             name: 'officeNumber',
+        },
+        {
+            // prompt user if they want to add employee
+            type: 'confirm',
+            message: "Would you like to add more team members?",
+            name: 'addEmployee',
         }
     ])
+    // set information about the prompts to the managerInfo
+    .then(managerInfo => {
+        const {name,id,email,officeNumber} = managerInfo;
+        const manager = new Manager(name,id,email,officeNumber)
+        team.push(manager)
+        console.log(manager)
+    })
 }
 // prompts for the Employee(choose betwen Intern & Engineer): Name, ID, Office Number, Email
 
