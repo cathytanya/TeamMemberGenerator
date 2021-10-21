9cbfa645e60664c185046901f113eb06fb3d1121
cathytanya <cathycolette.tanya@gmail.com>
2021-10-20 22:52:04 -0400

    wrote pseudocode for the index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// List of modules used
+const fs = require("fs")
+const inquirer = require("inquirer")
+
+
+// make an array for the entered positions the user will have
+
+// prompts for the Manager: Name, ID, Office Number, Email
+
+// prompts for the Employee(choose betwen Intern & Engineer): Name, ID, Office Number, Email
+
+// function to write the html file 
+
+// adding method to include additional employees
